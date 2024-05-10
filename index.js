3661f176813f8b8f7770b462526eda188e336d00
syedumarhusain <syedumarhusain1@gmail.com>
2024-05-10 18:59:23 +0530

    svg

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const port = 3000
 
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/header.html');
 })
